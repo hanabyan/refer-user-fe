84c71c44758f341519b641a28e3253aba59fbf0a
Budi Setiawan <bud1.setia1@gmail.com>
2019-09-17 19:00:53 +0700

    draw ads in dashboard

--- src/pages/Dashboard/PageDashboard.vue
+++ src/pages/Dashboard/PageDashboard.vue
@@ -1,6 +1,50 @@
 <template>
   <q-page padding>
-    <!-- content -->
+    <div class="text-h5 q-mb-md">Ads</div>
+
+    <div class="row q-col-gutter-md">
+      <div class="col-md-3">
+        <a href="https://www.google.com" target="_blank">
+          <q-card
+            class="my-card"
+            style="width: 240px; height: 150px; background-color: #eee;"
+          >
+          </q-card>
+        </a>
+      </div>
+
+      <div class="col-md-3">
+        <a href="https://www.google.com" target="_blank">
+          <q-card
+            class="my-card"
+            style="width: 240px; height: 150px; background-color: #eee;"
+          >
+          </q-card>
+        </a>
+      </div>
+
+      <div class="col-md-3">
+        <a href="https://www.google.com" target="_blank">
+          <q-card
+            class="my-card"
+            style="width: 240px; height: 150px; background-color: #eee;"
+          >
+          </q-card>
+        </a>
+      </div>
+
+      <div class="col-md-3">
+        <a href="https://www.google.com" target="_blank">
+          <q-card
+            class="my-card"
+            style="width: 240px; height: 150px; background-color: #eee;"
+          >
+          </q-card>
+        </a>
+      </div>
+    </div>
+
+    <!-- <div class="text-h5 q-mb-md q-mt-xl">Featured</div> -->
   </q-page>
 </template>
 
